a2a8a951a1518279c78aaaf48a9b08f1e3c425dd
John Nunemaker <nunemaker@gmail.com>
2018-03-25 14:26:23 -0400

    Shuffle some specs around and add specs for config options that were not tested

--- spec/flipper/cloud/configuration_spec.rb
+++ spec/flipper/cloud/configuration_spec.rb
@@ -23,12 +23,6 @@ RSpec.describe Flipper::Cloud::Configuration do
     expect(instance.url).to eq("http://localhost:5000/adapter")
   end
 
-  it "can set instrumenter" do
-    instrumenter = Object.new
-    instance = described_class.new(required_options.merge(instrumenter: instrumenter))
-    expect(instance.instrumenter).to be(instrumenter)
-  end
-
   it "can set read_timeout" do
     instance = described_class.new(required_options.merge(read_timeout: 5))
     expect(instance.read_timeout).to eq(5)
@@ -39,18 +33,20 @@ RSpec.describe Flipper::Cloud::Configuration do
     expect(instance.open_timeout).to eq(5)
   end
 
+  it "can set instrumenter" do
+    instrumenter = Object.new
+    instance = described_class.new(required_options.merge(instrumenter: instrumenter))
+    expect(instance.instrumenter).to be(instrumenter)
+  end
+
+  it "can set debug_output" do
+    instance = described_class.new(required_options.merge(debug_output: STDOUT))
+    expect(instance.debug_output).to eq(STDOUT)
+  end
+
   it "can set sync_interval" do
     instance = described_class.new(required_options.merge(sync_interval: 1_000))
     expect(instance.sync_interval).to eq(1_000)
-  end
-
-  it "can set producer_options" do
-    producer_options = {
-      flush_interval: 60,
-    }
-    options = required_options.merge(producer_options: producer_options)
-    instance = described_class.new(options)
-    expect(instance.producer_options[:flush_interval]).to eq(60)
   end
 
   it "passes sync_interval into sync adapter" do
@@ -61,9 +57,15 @@ RSpec.describe Flipper::Cloud::Configuration do
     expect(instance.adapter.synchronizer.interval).to be(1_000)
   end
 
-  it "can set debug_output" do
-    instance = described_class.new(required_options.merge(debug_output: STDOUT))
-    expect(instance.debug_output).to eq(STDOUT)
+  it 'defaults local_adapter' do
+    instance = described_class.new(required_options)
+    expect(instance.local_adapter).to be_instance_of(Flipper::Adapters::Memory)
+  end
+
+  it 'can set local_adapter' do
+    local_adapter = Flipper::Adapters::Memory.new
+    instance = described_class.new(required_options.merge(local_adapter: local_adapter))
+    expect(instance.local_adapter).to be(local_adapter)
   end
 
   it "defaults adapter block" do
@@ -83,5 +85,14 @@ RSpec.describe Flipper::Cloud::Configuration do
       Flipper::Adapters::Instrumented.new(adapter)
     end
     expect(instance.adapter).to be_instance_of(Flipper::Adapters::Instrumented)
+  end
+
+  it "can set producer_options" do
+    producer_options = {
+      flush_interval: 60,
+    }
+    options = required_options.merge(producer_options: producer_options)
+    instance = described_class.new(options)
+    expect(instance.producer_options[:flush_interval]).to eq(60)
   end
 end
