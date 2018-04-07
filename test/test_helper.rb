ff9db995f36a41fb78efa112d40b1dc23f54eea7
John Nunemaker <nunemaker@gmail.com>
2018-04-07 13:58:46 -0400

    Add missing pathname require

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'flipper'
 require 'minitest/autorun'
 require 'minitest/unit'
+require 'pathname'
+
 Dir['./lib/flipper/test/*.rb'].each { |f| require(f) }
 
 FlipperRoot = Pathname(__FILE__).dirname.join('..').expand_path
