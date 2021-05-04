f3235769072f57039e4176f3e4431dabf1684c5e
Brandon Keepers <brandon@opensoul.org>
2021-05-04 08:35:13 -0400

    Move memoizer class to right directory

--- lib/flipper/memoizer.rb
+++ lib/flipper/memoizer.rb
@@ -1,23 +1,22 @@
 module Flipper
-  # A utility class to manage the state of memoization
+  # Private: A utility class to manage the state of memoization
   class Memoizer
     def initialize(adapter)
       @adapter = Adapters::Memoizable.new(adapter)
       reset
     end
 
-    # Public: Returns a memoizing adapter or the original adapter based on the
-    # state of `memoizing?`
+    # Returns a memoizing adapter or the original adapter based on `memoizing?`
     def adapter
       memoizing? ? @adapter : @adapter.adapter
     end
 
-    # Public: Boolean indicating if memoization is currently happening
+    # Boolean indicating if memoization is currently happening
     def memoizing?
       @memoizing
     end
 
-    # Private: Call the block and reset when it completes
+    # Call the  block with memoization enabled, and then reset when it completes
     def call(&block)
       @memoizing = true
       @reset_later = false
@@ -27,7 +26,7 @@ module Flipper
       reset unless @reset_later
     end
 
-    # Public: Reset the state of the memoizer and clear the cache
+    # Reset the state of the memoizer and clear the cache
     def reset
       @memoizing = false
       @adapter.cache.clear
